e4fcdfdfb928621832bb33d393b00a04184eb11b
Jon Moorman <moorman.jon@gmail.com>
2013-03-05 22:26:02 -0500

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 FirstApp::Application.routes.draw do
-   get "static_pages/home"
-   get "static_pages/help"
-   get "static_pages/about"
-   get "static_pages/contact"
+   
+   root to: 'static_pages#home'
+  
+   match '/help',    to: 'static_pages#help'
+   match '/about',   to: 'static_pages#about'
+   match '/contact', to: 'static_pages#contact'
 
-  resources :microposts
+   resources :microposts
 
 
-  resources :users
+   resources :users
 
 
   # The priority is based upon order of creation:
@@ -57,9 +59,6 @@ FirstApp::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
